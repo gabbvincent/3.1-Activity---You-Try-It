fbe36900839b89e43976c9ea11950501099fed14
Vincent Gabb <vincentgabb@gmail.com>
2020-03-26 22:33:59 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func Ten_D(){
+  fmt.Println("Well, me and Kage are hungryWe're hungry for some fruitWe wander through the gardenIt would be a hootTo eat some low hangin' fruit.")
+}
+
+func main() {
+  
+  Ten_D()
+  
+}
